478d47dbed01df8e7b6dd2482b828f38a7141d02
Quinntyne Brown <quinntynebrown@gmail.com>
2025-12-19 16:38:39 -0500

    update

--- src/Quinntyne.Models/Content/GetContentsRequest.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Quinntyne Brown. All Rights Reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-using MediatR;
-
-namespace Quinntyne.Models.Content;
-
-public class GetContentsRequest: IRequest<GetContentsResponse> { }
